7669a9f341c6bdbc4ba3380d0fb9cc39003a2532
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-09-17 15:05:22 +0200

    first commit

---
+++ src/test/java/de/supercode/superBnB/SuperBnBApplicationTests.java
@@ -0,0 +1,13 @@
+package de.supercode.superBnB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperBnBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
