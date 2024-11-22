2db1cdf5f129711a714a1641ce8917d49715d4ec
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-11-22 10:20:57 +0100

    add userShortDto

--- src/main/java/de/supercode/superBnB/mappers/UserDtoMapper.java
+++ src/main/java/de/supercode/superBnB/mappers/UserDtoMapper.java
@@ -1,15 +1,15 @@
 package de.supercode.superBnB.mappers;
 
 import de.supercode.superBnB.dtos.UserResponseDto;
+import de.supercode.superBnB.dtos.UserShortDto;
 import de.supercode.superBnB.entities.user.User;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
 
 @Service
-public class UserDtoMapper implements Function<User, UserResponseDto> {
-    @Override
-    public UserResponseDto apply(User user) {
+public class UserDtoMapper{
+    public static UserResponseDto mapToDto(User user) {
         return new UserResponseDto(
                 user.getId(),
                 user.getRole(),
@@ -23,6 +23,17 @@ public class UserDtoMapper implements Function<User, UserResponseDto> {
                 user.getUserProfile().getAddress().getZipCode(),
                 user.getUserProfile().getAddress().getCity(),
                 user.getUserProfile().getAddress().getCountry()
+        );
+    }
+
+
+    public static UserShortDto mapToShortDto(User user) {
+        return new UserShortDto(
+                user.getId(),
+                user.getUserProfile().getFirstName(),
+                user.getUserProfile().getLastName(),
+                user.getUserProfile().getDateOfBirth(),
+                user.getUserProfile().getPhoneNumber()
         );
     }
 }
