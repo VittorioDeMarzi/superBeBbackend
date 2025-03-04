0c2596aa2fb3c159451d3aa9ab7d31d5d8049571
VittorioDeMarzi <vittoriodm7@gmail.com>
2025-03-04 22:02:56 +0100

    fix env for testing and cd/ci

--- src/test/java/de/supercode/superBnB/SuperBeBApplicationTests.java
+++ src/test/java/de/supercode/superBnB/SuperBeBApplicationTests.java
@@ -1,13 +1,26 @@
 package de.supercode.superBnB;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
+@ActiveProfiles("prod")
+@TestConfiguration
+@ImportAutoConfiguration(exclude = {MailSenderAutoConfiguration.class})
 class SuperBeBApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
